817bfa58888ca5455149233b23aecb78e9f20181
Asephara <e6292743@cmaisonneuve.qc.ca>
2025-02-20 16:05:31 +0000

    ajout exemple de web

---
+++ exemple.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int tableau[]= {25,26,27,28};
+    int taille = sizeof(tableau)/ sizeof(tableau[0]);
+    int i;
+
+for (i-0; i < taille; i ++);{
+    printf("%d\n", tableau[i] );
+}
+}
